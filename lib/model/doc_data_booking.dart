8044e8d764aefa214cba5824b942941f5e94ae30
Waad Nasr <132634512+wa3dnagy@users.noreply.github.com>
2025-02-14 16:18:39 +0200

    Delete lib directory

--- lib/model/doc_data_booking.dart
+++
@@ -1,6 +0,0 @@
-class DocDataBooking {
-    final String doctorName;
-     final String kind;
-       final String price ;
-       DocDataBooking({required this.doctorName, required  this.kind,required this.price, });
-}
